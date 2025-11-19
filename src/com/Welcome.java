40d4f50f93cc34e8b41afc233ea545a3bad37290
saroj <er.saroj.rath4u@gmail.com>
2025-11-19 23:24:08 +0530

    code changes from local elipse line no 7

--- src/com/Welcome.java
+++ src/com/Welcome.java
@@ -4,6 +4,7 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to java developer lovers");
+		System.out.println("code change from local eclipse line no7 ");
 	}
 
 }

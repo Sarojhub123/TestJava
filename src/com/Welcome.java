3808de6ba8679d4290f703c4541d613902dd58e6
Sarojhub123 <129937812+Sarojhub123@users.noreply.github.com>
2025-11-19 23:32:25 +0530

    merge conflict resolved from github

--- src/com/Welcome.java
+++ src/com/Welcome.java
@@ -4,11 +4,9 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to java developer lovers");
-<<<<<<< HEAD
 		System.out.println("code change from local eclipse line no7 ");
-=======
 		System.out.println("code changes from github");
->>>>>>> d4ecc3d0b7219c58a56976ef4827401472e6fec7
+
 	}
 
 }

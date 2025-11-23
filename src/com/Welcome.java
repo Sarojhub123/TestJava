d6939957d3bf5cc0b71b1283066683691bb60a28
saroj <er.saroj.rath4u@gmail.com>
2025-11-23 21:02:09 +0530

    code changes from local

--- src/com/Welcome.java
+++ src/com/Welcome.java
@@ -5,7 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome to java developer lovers");
 		System.out.println("code changes from local eclips only");
-		System.out.println("git command by using gui");
+		System.out.println("git command by using gui ");
 	}
 
 }
